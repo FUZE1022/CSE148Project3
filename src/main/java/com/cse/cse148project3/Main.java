4a6dd52fc4263646cdab960180f93e3ac4514f66
FUZE1022 <riccj28@mail.sunysuffolk.edu>
2023-05-04 18:03:00 -0400

    Initial commit

--- src/main/java/com/cse/cse148project3/Main.java
+++ src/main/java/com/cse/cse148project3/Main.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("views/main-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("views/registerUserScene-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 500);
         stage.setTitle("BookStore");
         stage.setScene(scene);
