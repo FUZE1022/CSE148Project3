cd87448f846a271fdd55f4c77a57a899e5bdee84
FUZE1022 <riccj28@mail.sunysuffolk.edu>
2023-05-04 09:43:44 -0400

    Initial commit

--- src/main/java/com/cse/cse148project3/controller/RegisterNewUserSceneController.java
+++ src/main/java/com/cse/cse148project3/controller/RegisterNewUserSceneController.java
@@ -1,10 +1,20 @@
 package com.cse.cse148project3.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+
+import java.io.IOException;
 
 public class RegisterNewUserSceneController {
+
+    @FXML
+    private StackPane stackPane;
 
     @FXML
     private TextField regsiterNewUserSceneUsernameTextField;
@@ -24,7 +34,18 @@ public class RegisterNewUserSceneController {
         System.out.println("btn pressed");
     }
 
-    public void registerNewUserSceneCancelButtonOnAction() {
-        System.out.println("btn pressed");
+    @FXML
+    private void registerNewUserSceneCancelButtonOnAction(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/cse/cse148project3/views/main" +
+                "-view.fxml"));
+        Parent newSceneRoot = loader.load();
+        for (Node node : stackPane.getChildren()) {
+            if (node != newSceneRoot) {
+                node.setVisible(false);
+                node.setManaged(false);
+            }
+        }
+        stackPane.getChildren().add(newSceneRoot);
+        newSceneRoot.toFront();
     }
 }
--- src/main/java/com/cse/cse148project3/controller/UserLoginSceneController.java
+++ src/main/java/com/cse/cse148project3/controller/UserLoginSceneController.java
@@ -1,10 +1,20 @@
 package com.cse.cse148project3.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+
+import java.io.IOException;
 
 public class UserLoginSceneController {
+
+    @FXML
+    private StackPane stackPane;
 
     @FXML
     private TextField userLoginSceneUsernameTextField;
@@ -21,7 +31,18 @@ public class UserLoginSceneController {
         System.out.println("btn pressed");
     }
 
-    public void userLoginSceneCancelButtonOnAction() {
-        System.out.println("btn pressed");
+    @FXML
+    private void userLoginSceneCancelButtonOnAction(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/cse/cse148project3/views/main" +
+                "-view.fxml"));
+        Parent newSceneRoot = loader.load();
+        for (Node node : stackPane.getChildren()) {
+            if (node != newSceneRoot) {
+                node.setVisible(false);
+                node.setManaged(false);
+            }
+        }
+        stackPane.getChildren().add(newSceneRoot);
+        newSceneRoot.toFront();
     }
 }
